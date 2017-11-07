aa34f7fd3b2e2dc604dd03d5f175255b8e60a20e
Redko_Oleksii <aleks.redko@outlook.com>
2017-11-07 08:04:37 -0500

    start

--- profile.php
+++ profile.php
@@ -1,13 +1,24 @@
 <?php
 // fake $app, $log so that Netbeans can provide suggestions while typing code
-if (false) {
-    $app = new \Slim\Slim();
-    $log = new Logger('main');
-}
+//if (false) {
+//    $app = new \Slim\Slim();
+//    $log = new Logger('main');
+//}
 $app->get('/profile', function() use ($app) {
-    $profileList = array();
-    if ($_SESSION['user']) {
-        $profileList = DB::query('SELECT * FROM users WHERE Id=%i', $_SESSION['user']['id']);
+    
+
+    if (!$_SESSION['user']) {
+        $app->render('access_denied.html.twig');
+        return;
     }
-    $app->render('profile.html.twig', array('profileList' => $profileList));
+   $profileList = array();
+   $profileList = DB::query('SELECT * FROM users WHERE Id=%i', $_SESSION['user']['id']);
+    if (!$profileList) {
+        
+       
+       $app->render('not_found.html.twig');
+        return;
+    }
+     
+     $app->render('profile.html.twig', array('pl' => $profileList));
 });
