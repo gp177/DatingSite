8828b46ea80e6e8fbfa62cbdb73417c7dd029784
GP-PC\GP <Giorgioplescia1@gmail.com>
2017-11-08 19:18:47 -0500

    template updates

--- chat.php
+++ chat.php
@@ -6,9 +6,11 @@ if (false) {
 }
 
 
-$app->get('/chat/:chatId', function($id) use ($app) {
+$app->get('/chat', function() use ($app) {
    
-    $chatfetch = DB::queryFirstRow('SELECT * FROM chats WHERE id=%i', $id);
+    $chatfetch = DB::queryFirstRow('SELECT c.userId1, c.userId2, u1.username as u1username, u2.username as u2username,
+FROM users u1, chats c, users u2
+WHERE c.userId1 = u1.id AND c.userId2 = u2.id');
     if (!$chatfetch) {
         $app->render('not_found.html.twig');
         return;
