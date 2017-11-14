f72665b217be638956d29437e4b4c50ffb082e7f
STUDENTS\1795803 <1795803@johnabbottcollege.net>
2017-11-14 12:59:04 -0500

    chat working!!!!

--- chat.php
+++ chat.php
@@ -17,27 +17,15 @@ $app->get('/chats', function() use ($app) {
         echo'you don\'t havechats';
     }
     
-    
-    
-   
-//    $chatfetch = DB::query('SELECT c.userId1, c.userId2, u1.username as u1username, u2.username as u2username
-//FROM users u1, chats c, users u2
-//WHERE c.userId1 = u1.id AND c.userId2 = u2.id');
-//    
-   
-    
-//    if (!$chatfetch) {
-//        $app->render('not_found.html.twig');
-//        return;
-//    }
-    print_r($chatList);
+
+    //print_r($chatList);
     $app->render('chat.html.twig', array('chatList'=>$chatList));
 });
 
 $app->get('/chats(/:chatId)', function($chatId) use ($app){
 
 $chat=DB::query('SELECT * FROM chatMessages WHERE chatId=%s ', $chatId);
-print_r($chat);
+//print_r($chat);
 $app->render('messageBox.html.twig',array('mes'=>$chat));
 
 });
