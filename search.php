408c6a2c4fff24bd9f821427a7efc1ed85955dfb
STUDENTS\1795803 <1795803@johnabbottcollege.net>
2017-11-17 12:46:10 -0500

    class friday

--- search.php
+++ search.php
@@ -5,33 +5,6 @@ if (false) {
     $log = new Logger('main');
 }
 
-<<<<<<< HEAD
-$app->get('/search', function() use ($app, $log) {
-
-    if (!$_SESSION['user']) {
-        $app->render('access_denied.html.twig');
-        return;
-    } else {
-        
-        
-        $profileList = DB::queryFirstRow('SELECT *, YEAR(CURRENT_TIMESTAMP) - YEAR(birthDate) - (RIGHT(CURRENT_TIMESTAMP, 5) < RIGHT(birthDate, 5)) as age FROM users');
-         $userCount = DB::query('SELECT id FROM users');
-        
-        
-        if (!$profileList) {
-
-//     print_r($profileList); 
-            $app->render('not_found.html.twig');
-            return;
-        } else {
-  
-            $app->render('search_results.html.twig',array('list' => $profileList, 'userCount' => count($userCount)));
-           
-        }
-    }
-});
-
-=======
 
 $app->get('/search', function() use ($app) {
       
@@ -47,7 +20,7 @@ $app->post('/search', function() use ($app) {
    $toAge=$app->request()->post('ageTo');
    $photo = $app->request()->post('withPhoto');
    
-  print_r($photo);
+//  print_r($photo);
   if($photo!='photo'){
    $newList=DB::query('SELECT *,YEAR(CURRENT_TIMESTAMP) - YEAR(birthDate) - (RIGHT(CURRENT_TIMESTAMP, 5) < RIGHT(birthDate, 5)) as age FROM users
                         WHERE
@@ -61,6 +34,6 @@ $app->post('/search', function() use ($app) {
                         (YEAR(CURRENT_TIMESTAMP) - YEAR(birthDate) - (RIGHT(CURRENT_TIMESTAMP, 5) < RIGHT(birthDate, 5)))<=%s and 
                         gender=%s AND profilePicPath !="/images/nopic.png"',$fromAge,$toAge,$gender);
    }
-    $app->render('/search.html.twig', array('list' => $newList, 'age' => $age));
+    $app->render('/search_results.html.twig', array('list' => $newList, 'age' => $age));
 });
->>>>>>> 71f36738a9c9bb81ff0a6f74205c8e23cb82e877
+
